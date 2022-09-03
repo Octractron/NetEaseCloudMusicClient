aaa2655e8441900ef6d9f99739d7cc2ecadcca06
hotspringGG <78401087+hotspringGG@users.noreply.github.com>
2022-09-03 16:43:34 +0800

    commit

---
+++ CloudMusicApi.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+# ==========================
+import SpiderUtils.Search as Searcher
+from SpiderUtils.MusicSourceURL import GetSongSourceUrlDefault
+
+
+def Search(page, limit, question):
+    return Searcher.search(page, limit, question)
+
+def Get_source_URL(SongID):
+    GetSongSourceUrlDefault(SongId=SongID)
+
+def GET_Song_Id():
+    
